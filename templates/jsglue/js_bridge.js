5fe439b421219ec7dc1ad9d8a80edf5221450932
aidan <erhuabushuo@gmail.com>
2017-07-08 16:51:03 +0800

    add support for non-ASCII query paramaters

--- templates/jsglue/js_bridge.js
+++ templates/jsglue/js_bridge.js
@@ -66,7 +66,7 @@ var {{ namespace }} = new(function () {
                 } else {
                   url += '&';
                 }
-                url += r + '=' + rule[r];
+                url += r + '=' + encodeURI(rule[r]);
               }
             }
             if (has_anchor) {
